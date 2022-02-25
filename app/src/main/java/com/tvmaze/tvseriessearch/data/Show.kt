b262da15fce460163627d4cc006fcf1e64bfdc77
Filip Loster <fblost@gmail.com>
2022-02-25 17:42:52 +0100

    Added Show data classes

---
+++ app/src/main/java/com/tvmaze/tvseriessearch/data/Show.kt
@@ -0,0 +1,16 @@
+package com.tvmaze.tvseriessearch.data
+
+data class Show(
+    val id: Int,
+    val url: String,
+    val name: String,
+    val genres: List<String>,
+    val weight: Int
+) {
+}
+
+data class ShowWithScore(
+    val score: Float,
+    val show: Show
+) {
+}
